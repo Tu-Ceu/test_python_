b372916b63c94d28cdf6d8bdfbad87dda93d82d2
Dash Barrett <dbarrett7614@floridapoly.edu>
2025-11-12 21:47:22 +0000

    Added Computer Choice

--- game.py
+++ game.py
@@ -1,10 +1,9 @@
-import random
 print("welcome to Rock-Paper-Scissor") 
+
+import random
 
 def main():
     computer_options = ["rock", "paper", "scissors"]
     print(f"Computer chose: {computer_options[random.randint(0,2)]}")
-
 if __name__ == "__main__":
     main() 
-
